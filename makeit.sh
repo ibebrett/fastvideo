fcd30ccedda27ac9012522eed21906cc209751bf
Brett Jurman <i.be.brett@gmail.com>
2014-08-07 15:41:51 -0400

    removed silly build tools

--- makeit.sh
+++
@@ -1,3 +0,0 @@
-g++   -L/opt/local/lib  -I/usr/X11R6/include -L/usr/X11R6/lib  -lavformat -lavcodec -lswscale -lavutil -lSDLmain -Wl,-framework,AppKit -lSDL -Wl,-framework,Cocoa  -lm screenshot.cpp -lpthread -lX11 -o screenshot
-g++   -L/opt/local/lib  -I/usr/X11R6/include -L/usr/X11R6/lib  -lavformat -lavcodec -lswscale -lavutil -lSDLmain -Wl,-framework,AppKit -lSDL -Wl,-framework,Cocoa  -lm search.cpp -lpthread -lX11 -o search
-
--- makeit_linux.sh
+++
@@ -1,4 +0,0 @@
-export PKG_CONFIG_PATH=/home/ubuntu/ffmpeg_build/lib/pkgconfig:$PKG_CONFIG_PATH
-g++ screenshot.cpp -o screenshot $(pkg-config --libs --cflags libavformat libswscale) -lrt
-g++ search.cpp -o search $(pkg-config --libs --cflags libavformat libswscale) -lrt
-
