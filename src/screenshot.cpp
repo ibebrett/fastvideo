f44089792d5a3e96c3acc25a667240427ebe2816
Brett Jurman <i.be.brett@gmail.com>
2014-08-07 16:28:55 -0400

    Update screenshot.cpp

--- src/screenshot.cpp
+++ src/screenshot.cpp
@@ -1,22 +1,3 @@
-// tutorial01.c
-//
-// This tutorial was written by Stephen Dranger (dranger@gmail.com).
-//
-// Code based on a tutorial by Martin Bohme (boehme@inb.uni-luebeckREMOVETHIS.de)
-// Tested on Gentoo, CVS version 5/01/07 compiled with GCC 4.1.1
-
-// A small sample program that shows how to use libavformat and libavcodec to
-// read video from a file.
-//
-// Use the Makefile to build all examples.
-//
-// Run using
-//
-// tutorial01 myvideofile.mpg
-//
-// to write the first five frames from "myvideofile.mpg" to disk in PPM
-// format.
-
 #define __STDC_CONSTANT_MACROS
 
 extern "C" {
